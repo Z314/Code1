6cb9622268919b9bae5cb20f5b7e7e4a49687eec
John Patel <34202391+Z314@users.noreply.github.com>
2024-11-30 16:37:46 +1000

    Update drawing1.py

--- turtle1/drawing1.py
+++ turtle1/drawing1.py
@@ -1,1 +1,22 @@
 
+import turtle # The turtle module, part of the standard Python library
+import my_graphics # Custom module
+
+# Set up the screen
+screen = turtle.Screen()
+screen.bgcolor("white")  # Set background color
+
+# constants
+X1 = 0
+Y1 = 0
+# myColor = 'blue'
+myColor = my_graphics.rgb(255, 0, 0) # calling the rgb function from the my_graphics module 
+
+
+def main():
+    my_graphics.pattern2(X1, Y1, myColor) # calling the pattern2 function from the my_graphics module 
+
+
+main()
+# Keep the window open
+turtle.mainloop()
